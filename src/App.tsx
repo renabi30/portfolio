6bef796c06024dc87db6d4c62cdf732fef819c8c
Rena <renabi30@gmail.com>
2024-01-20 12:23:46 -0500

    basic website structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <p>Hi!</p>
+      <h1>I'm Rena Bi</h1>
+      <p>I am a software engineer currently based in the Baltimore-DC metropolitan area.
+      </p>
+    </>
+  )
+}
+
+/*
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+  const [count, setCount] = useState(0)
+
+<div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +39,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
+*/
 
 export default App
