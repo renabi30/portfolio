91ec9dccdcc604037140ced9501161a2fe05c1d8
Rena <renabi30@gmail.com>
2024-01-21 18:54:33 -0500

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import './App.css'
 import Header from "./components/header.tsx"
 import Home from "./components/home.tsx"
 import About from "./components/about.tsx"
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Home />} />
+          <Route path="/portfolio/about" element={<About />} />
         </Routes>
+      </BrowserRouter>
     </> 
   )
 }
