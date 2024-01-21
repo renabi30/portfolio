4c7fc5d367c81a81ba8cfe51d754e384f5c85a17
Rena <renabi30@gmail.com>
2024-01-21 18:51:41 -0500

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import './App.css'
 import Header from "./components/header.tsx"
 import Home from "./components/home.tsx"
 import About from "./components/about.tsx"
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </HashRouter>
     </> 
   )
 }
